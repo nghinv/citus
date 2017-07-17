5eb46e9c455e052bcd0dda650c386ded35f4c48a
Marco Slot <marco.slot@gmail.com>
2017-07-17 17:26:01 +0200

    Add function for dumping global wait edges

--- src/backend/distributed/citus--7.0-4--7.0-5.sql
+++ src/backend/distributed/citus--7.0-4--7.0-5.sql
@@ -16,3 +16,19 @@ LANGUAGE 'c' STRICT
 AS $$citus$$, $$dump_local_wait_edges$$;
 COMMENT ON FUNCTION pg_catalog.dump_local_wait_edges(int)
 IS 'returns a list of blocked transactions and the transaction they are waiting for';
+
+CREATE FUNCTION pg_catalog.dump_global_wait_edges(
+                    OUT waiting_pid int4,
+                    OUT waiting_node_id int4,
+                    OUT waiting_transaction_num int8,
+                    OUT waiting_transaction_stamp timestamptz,
+                    OUT blocking_pid int,
+                    OUT blocking_node_id int4,
+                    OUT blocking_transaction_num int8,
+                    OUT blocking_transaction_stamp timestamptz,
+                    OUT blocking_transaction_waiting bool)
+RETURNS SETOF RECORD
+LANGUAGE 'c' STRICT
+AS $$citus$$, $$dump_global_wait_edges$$;
+COMMENT ON FUNCTION pg_catalog.dump_global_wait_edges()
+IS 'returns a list of blocked transactions and the transaction they are waiting for';
