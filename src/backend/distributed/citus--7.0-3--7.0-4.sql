c0acc59e51bc2458305dbe00142d8cafbab5d975
Brian Cloutier <github@briancloutier.com>
2017-07-17 15:20:58 +0300

    Add comment on adding values to noderole

--- src/backend/distributed/citus--7.0-3--7.0-4.sql
+++ src/backend/distributed/citus--7.0-3--7.0-4.sql
@@ -6,6 +6,12 @@ CREATE TYPE noderole AS ENUM (
 	'primary',     -- node is available and accepting writes
 	'secondary',   -- node is available but only accepts reads
 	'unavailable' -- node is in recovery or otherwise not usable
+-- adding new values to a type inside of a transaction (such as during an ALTER EXTENSION
+-- citus UPDATE) isn't allowed in PG 9.6, and only allowed in PG10 if you don't use the
+-- new values inside of the same transaction. You might need to replace this type with a
+-- new one and then change the column type in pg_dist_node. There's a list of
+-- alternatuves here:
+-- https://stackoverflow.com/questions/1771543/postgresql-updating-an-enum-type/41696273
 );
 
 ALTER TABLE pg_dist_node ADD COLUMN noderole noderole NOT NULL DEFAULT 'primary';
