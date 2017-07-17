ec1640ca8eaae90bed940eb4c58845dcc2f46bd0
Brian Cloutier <github@briancloutier.com>
2017-07-17 17:40:47 +0300

    fix typo

--- src/backend/distributed/citus--7.0-3--7.0-4.sql
+++ src/backend/distributed/citus--7.0-3--7.0-4.sql
@@ -10,7 +10,7 @@ CREATE TYPE pg_catalog.noderole AS ENUM (
 -- citus UPDATE) isn't allowed in PG 9.6, and only allowed in PG10 if you don't use the
 -- new values inside of the same transaction. You might need to replace this type with a
 -- new one and then change the column type in pg_dist_node. There's a list of
--- alternatuves here:
+-- alternatives here:
 -- https://stackoverflow.com/questions/1771543/postgresql-updating-an-enum-type/41696273
 );
 
